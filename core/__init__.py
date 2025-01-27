7990366cc183b3157e8a1f1ad79ad6bc7e7e8a5f
MajorVick <126904019+MajorVick@users.noreply.github.com>
2025-01-27 19:37:27 +0000

    Added docker config and updated requirements.

--- core/__init__.py
+++ core/__init__.py
@@ -6,7 +6,7 @@ from sqlalchemy.engine import Engine
 from sqlite3 import Connection as SQLite3Connection
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./store.sqlite3'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///app.db')
 app.config['SQLALCHEMY_ECHO'] = False
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
